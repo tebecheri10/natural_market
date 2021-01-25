649735e3faf798ebdc941f81d94a71e14c32beb6
tebecheri10 <tebecheri10@gmail.com>
2021-01-24 22:42:30 -0300

    listo para cuarta entrega

---
+++ js/codigos.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.contenedorMain');
+ScrollReveal().reveal('.tituloTestimonios');
+ScrollReveal().reveal('.contenedorTestimonios');
+ScrollReveal().reveal('.tituloSuscriptores');
+ScrollReveal().reveal('.listaBeneficios');
+ScrollReveal().reveal('.textoBanner');
+ScrollReveal().reveal('.botonSuscribir');
+ScrollReveal().reveal('.redesSociales');
+
+
+
